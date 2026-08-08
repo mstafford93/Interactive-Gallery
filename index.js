5b2c382e051fe178e99024539ec219237162c324
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-08 12:09:02 +0100

    fixed errors of not a function

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 function addImage(){
     //HTML elements
-    const imageInput = document.querySelector("input"); //Can't access value in getElementById
+    const imageInput = document.querySelector("input").value; //Can't access value in getElementById
     const addImageBtn = document.getElementById("addImageBtn");
     const imageDisplay = document.getElementById("imageDisplay");
 
-    if (imageInput.value){
+    if (imageInput){
+
         //Create new element for image
-        const newImage = imageDisplay.createElement("div");
+        const newImage = document.createElement("div");
         newImage.classList.add("newImage");
 
         //Accessing the input value
-        newImage.innerHTML = `<img src = "${imageInput.value}".jpg>`;
+        newImage.innerHTML = `<img src = "${imageInput}".jpg>`;
 
         //Create remove button
         const removeBtn = document.createElement("button");
-        removeBtn.textContent("Remove");
+        removeBtn.textContent = 'Remove';
         removeBtn.classList.add("removeBtn");
-        removeBtn.onclick(() => {
-            imageDisplay.removeChild("newImage");
-        });
+        removeBtn.onclick = () => imageDisplay.removeChild("newImage");
 
-    }
-}
+    } else {
+        alert("Please enter image name");
+    };
+};
+
+document.getElementById("addImageBtn").addEventListener("click", addImage);
