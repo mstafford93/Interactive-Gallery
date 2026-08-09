aa634805cad8e16712a4aa6a38930c574bf73433
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-09 09:36:27 +0100

    append button and img

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ function addImage(){
         newImage.classList.add("newImage");
 
         //Accessing the input value
-        newImage.innerHTML = `<img src = "${imageInput}".jpg>`;
+        newImage.innerHTML = `<img src = "${imageInput}.jpg">`;
 
         //Create remove button
         const removeBtn = document.createElement("button");
         removeBtn.textContent = 'Remove';
         removeBtn.classList.add("removeBtn");
         removeBtn.onclick = () => imageDisplay.removeChild("newImage");
+
+        //Append image and button
+        imageDisplay.appendChild(newImage);
+        imageDisplay.appendChild(removeBtn);
 
     } else {
         alert("Please enter image name");
